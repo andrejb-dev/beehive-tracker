d188bc90156e0ea10974cdbc780906ddf344e183
Zavael <test@test.com>
2015-08-10 16:21:08 +0200

    displaying first collection of yards on gui

--- beehive-gui/src/main/java/sk/badand/beehive/gui/controller/YardListController.java
+++ beehive-gui/src/main/java/sk/badand/beehive/gui/controller/YardListController.java
@@ -7,7 +7,15 @@ package sk.badand.beehive.gui.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableColumn.CellDataFeatures;
+import javafx.scene.control.TableView;
+import javafx.util.Callback;
+import sk.badand.beehive.modelfx.YardFx;
+import sk.badand.beehive.services.YardService;
 
 /**
  * FXML Controller class
@@ -15,7 +23,16 @@ import javafx.fxml.Initializable;
  * @author abadinka <andrej.badinka@interway.sk>
  */
 public class YardListController implements Initializable, ScreenControllerInjectable {
+
     private ScreensController controller;
+    private YardService yardService = YardService.getInstance();
+
+    @FXML
+    private TableView<YardFx> yardsView;
+    @FXML
+    private TableColumn<YardFx, String> yardName;
+    @FXML
+    private TableColumn<YardFx, Integer> hivesCount;
 
     /**
      * Initializes the controller class.
@@ -23,11 +40,14 @@ public class YardListController implements Initializable, ScreenControllerInject
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+        yardName.setCellValueFactory(data -> data.getValue().name());
+        hivesCount.setCellValueFactory(data -> data.getValue().hivesCount());
+        yardsView.setItems(yardService.getYards());
+    }
 
     @Override
     public void setScreenController(ScreensController screenController) {
         this.controller = screenController;
     }
-    
+
 }
--- beehive-gui/src/main/java/sk/badand/beehive/modelfx/YardFx.java
+++ beehive-gui/src/main/java/sk/badand/beehive/modelfx/YardFx.java
@@ -3,13 +3,19 @@
  */
 package sk.badand.beehive.modelfx;
 
+import java.util.Map;
 import javafx.beans.property.ListProperty;
 import javafx.beans.property.MapProperty;
 import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.ReadOnlyIntegerProperty;
+import javafx.beans.property.ReadOnlyIntegerWrapper;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.beans.property.SimpleListProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import sk.badand.beehive.model.Hive;
 import sk.badand.beehive.model.Yard;
 import sk.badand.beehive.model.enums.SunExposure;
 
@@ -22,7 +28,7 @@ public class YardFx {
     
     private ObjectProperty<AddressFx> address = new SimpleObjectProperty<>();
     private ObjectProperty<EnvironmentFx> environment = new SimpleObjectProperty<>();
-    private ListProperty<HiveFx> hives = new SimpleListProperty<>();
+    private ListProperty<HiveFx> hives = new SimpleListProperty<>(FXCollections.observableArrayList());
     private ObjectProperty<SunExposure> sunExposure = new SimpleObjectProperty<>();
     private StringProperty name = new SimpleStringProperty();
 
@@ -31,8 +37,26 @@ public class YardFx {
         address.setValue(new AddressFx(origin.getAddress()));
         environment.setValue(new EnvironmentFx(origin.getEnvironment()));
         sunExposure.setValue(origin.getSunExposure());
-//        hives = new SimpleListProperty<>
+        for (Map.Entry<String, Hive> hive : origin.getHives().entrySet()) {
+            hives.add(new HiveFx(hive.getValue()));
+        }
         name.setValue(origin.getName());
+    }
+
+    public ObjectProperty<AddressFx> address() {
+        return address;
+    }
+
+    public ListProperty<HiveFx> hives() {
+        return hives;
+    }
+    
+    public ReadOnlyObjectWrapper hivesCount(){
+        return new ReadOnlyObjectWrapper(hives.size());
+    }
+
+    public StringProperty name() {
+        return name;
     }
     
     
